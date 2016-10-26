cfd774c7805b11a0d11900da49f980a98f34e5f9
Lee Cook <lee@stadion.io>
2016-10-26 15:57:41 +0100

    initial release

---
+++ Stadion.Football.DataProcessorWebJob/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Stadion.DataProviders.Opta.Football.WebJobs;
+
+namespace FootballDataProcessorWebJob
+{
+    public class Program 
+    {
+      
+        static void Main(string[] args)
+        {
+
+            JobHost host = new JobHost();
+
+            host.RunAndBlock();
+        }
+
+        public static void ProcessQueueMessage([QueueTrigger("footballdata")] CloudQueueMessage binaryFile, TextWriter logger)
+        {
+
+            WebjobDataProcessor dataProcessor = new WebjobDataProcessor();
+            dataProcessor.ProcessMessage(binaryFile);
+        }
+
+
+    }
+}
